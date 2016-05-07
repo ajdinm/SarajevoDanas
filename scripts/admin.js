18bb5b149d2a5526bdb93667860adbb012f71bd8
Ajdin <ajdin.m@gmail.com>
2016-05-08 00:11:40 +0200

    Fixed client side validation to redirect. Included both client and server side validation for country code and calling number

--- scripts/admin.js
+++ scripts/admin.js
@@ -3,10 +3,31 @@ window.onload = function () {
         var path = document.getElementById("img_path");
         var alt = document.getElementById("img_alt");
         var text = document.getElementById("text");
+        var ccode = document.getElementById("ccode");
+        var phone = document.getElementById("phone");
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            var codeOK = false;
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                  var resp = JSON.parse(xhttp.responseText);
+                  if (phone.value.startsWith(resp['callingCodes'])) {
+                      codeOK = true;
+                  }
+                if(!codeOK) {
+                    alert("Neispravan pozivni broj.");
+                    window.location  = "./../pages/admin.php?what=create";
+                    event.returnValue = false;
+                }
+              }
+            }
+            xhttp.open("GET", "https://restcountries.eu/rest/v1/alpha?codes=" + ccode, true);
+            xhttp.send();
 
         if(path.value.length == 0 || alt.value.length == 0 || text.value.length == 0) {
-            alert('Nisu uneseni svi potrebni parametri!');
-            window.location  = "./../pages/admin.php";
+//            alert('Nisu uneseni svi potrebni parametri!');
+            window.location  = "./../pages/admin.php?what=create";
+            event.returnValue = false;
         }
 
     }
--- scripts/admin.php
+++ scripts/admin.php
@@ -16,10 +16,38 @@
                     $img_path = htmlentities($_GET['img_path'], ENT_QUOTES);
                     $img_alt = htmlentities($_GET['img_alt'], ENT_QUOTES);
                     $text = htmlentities($_GET['text'], ENT_QUOTES);
-
+                    $ccode = htmlentities($_GET['coutry_code'], ENT_QUOTES);
+                    $phone = htmlentities($_GET['phone'], ENT_QUOTES);
                     $news_row = $img_path.','.$img_alt.','.time().','.$text;
 
-                    file_put_contents('./../data/test.csv', $news_row. PHP_EOL, FILE_APPEND);
+                    $service_url = 'https://restcountries.eu/rest/v1/alpha?codes=ba';
+                    $curl = curl_init($service_url);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    $curl_response = curl_exec($curl);
+                    if ($curl_response === false) {
+                        $info = curl_getinfo($curl);
+                        curl_close($curl);
+                        die('error occured during curl exec. Additioanl info: ' . var_export($info));
+                    }
+                    curl_close($curl);
+                    $decoded = json_decode($curl_response, true);
+                    if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+                        die('error occured: ' . $decoded->response->errormessage);
+                    }
+                    $validateCCode = false;
+                    foreach($decoded as $country) {
+                        foreach($country['callingCodes'] as $code) {
+                            if(substr($phone, 0, strlen($code)) === $code) {
+                                $validateCCode = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(!$validateCCode) {
+                        die('Nekonzistentan kod drzave i pozivni');
+                    }
+
+                    //file_put_contents('./../data/test.csv', $news_row. PHP_EOL, FILE_APPEND);
                 }
             }
         }
@@ -27,4 +55,31 @@
     else {
         header('Location: ./../pages/login.php');
     }
+
+    function CallAPI($method, $url, $data = false) {
+    $curl = curl_init();
+
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
 ?>
